7c11c6d576f12aa190f632eb7bbb56bd0f203f60
Bianca91 <biancasteyn91@gmail.com>
2018-05-02 22:45:04 +0200

    Add typescript

---
+++ target/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const routing_controllers_1 = require("routing-controllers");
+const products_1 = require("./controller/products");
+const port = process.env.PORT || 4000;
+const app = routing_controllers_1.createKoaServer({
+    controllers: [products_1.default]
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
+require("reflect-metadata");
+const port = process.env.PORT || 4000;
+const app = routing_controllers_1.createKoaServer({
+    controllers: [products_1.default]
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
+//# sourceMappingURL=index.js.map
